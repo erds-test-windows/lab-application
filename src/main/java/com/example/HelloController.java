fde621bb7453970cbbc57dfd5d5792d54d668cc1
Craig <craigerdmann@hotmail.com>
2017-04-21 06:59:59 -0700

    Adding Chuck Norris db

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,12 +2,17 @@ package com.example;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HelloController {
 
   @RequestMapping("/")
   public String index() {
-    return "Greetings from the Spring Boot Starter App!";
+    RestTemplate restTemplate = new RestTemplate();
+    String url = "http://api.icndb.com/jokes/random?exclude=[explicit]";
+
+    return restTemplate.getForObject(url, String.class);
   }
 }
